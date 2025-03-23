deb511bfe4575d034951d9132b6a99b1e9ea059c
xamidev <xamidev@riseup.net>
2025-03-23 20:18:28 +0100

    vga_print

--- vga.s
+++ vga.s
@@ -11,6 +11,19 @@ vga_clear:
 
 ; IN:  <si> : pointer to null-terminated string
 ; OUT: None
+vga_print:
+	.loop:
+		lodsb
+		or al, al
+		jz .done
+		mov ah, 0x0E
+		int 0x10
+		jmp .loop
+	.done:
+		ret
+
+; IN:  <si> : pointer to null-terminated string
+; OUT: None
 vga_println:
 	.loop:
 		lodsb
