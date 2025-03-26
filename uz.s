67a58e86401fefda5a8a3d00ceab8ce4b764adb1
xamidev <xamidev@riseup.net>
2025-03-26 14:40:25 +0100

    reboot and help

--- uz.s
+++ uz.s
@@ -1,11 +1,23 @@
 ; uzbootsc - Stage 2 bootloader
 ; This is free and unencumbered software released into the public domain.
 
+; todo: macros (to make code smaller) for regular calls
+; todo: improve calls w/ stack
+; todo: declare macros for ENDL 
+
+
+; **************
+; *** macros ***
+; **************
+
+%define ENDL 0xA, 0xD
+
 ; loaded at this addr by stage1
 org 0x8000
 bits 16
 
 ; greetings
+call vga_clear
 mov si, msg_itworks
 call vga_println
 
@@ -27,6 +39,18 @@ shell_loop:
 	mov di, cmd_help
 	call str_cmp
 	jc .help
+
+	; command: reboot
+	mov si, buffer
+	mov di, cmd_reboot
+	call str_cmp
+	jc .reboot
+
+	; command: what
+	mov si, buffer
+	mov di, cmd_what
+	call str_cmp
+	jc .what
 	
 	mov si, msg_badcmd
 	call vga_println
@@ -40,6 +64,19 @@ shell_loop:
 	mov si, msg_help
 	call vga_println
 	jmp shell_loop
+
+.reboot:
+	int 19h
+	; we shouldnt reach this
+	mov si, err_reboot
+	call vga_println
+	jmp shell_loop
+
+.what:
+	mov si, msg_what
+	call vga_println	
+	jmp shell_loop
+		
 
 ; **************
 ; *** string ***
@@ -135,7 +172,7 @@ str_cmp:
 
 vga_clear:
 	mov ah, 0
-	mov al, 2
+	mov al, 3
 	int 0x10
 	ret
 
@@ -169,15 +206,46 @@ vga_println:
 		int 0x10
 		ret
 
+; dumb shit
+vga_print_hex:
+	push ax
+	push cx
+	push dx
+	
+	mov cx, 4
+.hex_loop:
+	rol bx, 4
+	mov al, bl
+	and al, 0x0F
+	cmp al, 10
+	jl .hex_digit
+	add al, 'A' - 10
+	jmp .print_digit
+.hex_digit:
+	add al, '0'
+.print_digit:
+	mov ah, 0x0E
+	int 10h
+	loop .hex_loop
+
+	pop dx
+	pop cx
+	pop ax
+	ret
+
 ; ****************
 ; *** messages ***
 ; ****************
 
-msg_itworks	db	"addr 0x8000", 0
+msg_itworks	db	"uzbootsc version 1 - addr 0x8000", 0
 msg_prompt	db	">", 0
-msg_help	db	"read the source you fucking moron", 0
-msg_badcmd	db	"Unknown command, rtfm", 0
+msg_help	db	"help reboot what", 0
+msg_badcmd	db	"Unknown command", 0
+msg_what	db	"[UZLESS BOOTSECTOR]", ENDL, ENDL, "I. Why?", ENDL, ENDL, "yes", 0
+err_reboot	db	"Error during reboot sequence", 0
 
 cmd_help	db	"help", 0
+cmd_reboot	db	"reboot", 0
+cmd_what	db	"what", 0
 
 buffer		times 64 db 0
